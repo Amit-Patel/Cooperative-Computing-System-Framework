bab4b01f40174803c898a069868c204c132ac063
unknown <aswin6197@gmail.com>
2018-11-04 21:07:51 +0530

    solving merge issues

--- project_code.py
+++ project_code.py
@@ -101,69 +101,6 @@ class Task:
     def get_req(self):
         return self.req
 
-class Node:
-
-    def __init__(self,cid,ctid,stat):
-        self.cid = cid
-        self.ctid = ctid
-        self.stat = stat
-
-    def set_clusterid(self,value):
-        self.cid = value
-
-    def set_currenttaskid(self,value):
-        self.ctid = value
-
-    def set_status(self, value):
-        self.stat = value
-
-    def get_clusterid(self):
-        return self.cid
-
-    def get_currenttaskid(self):
-        return self.ctid
-
-    def get_nodestatus(self):
-        return self.stat
-
-class Task:
-
-    def __init__(self,tid,pr,at,et,req):
-        self.tid = tid
-        self.pr = pr
-        self.at = at
-        self.et = et
-        self.req = req
-
-    def set_taskid(self, value):
-        self.tid = value
-
-    def set_taskpriority(self, value):
-        self.pr = value
-
-    def set_arrivaltime(self, value):
-        self.at = value
-
-    def set_et(self,value):
-        self.et = value
-
-    def set_req(self,value):
-        self.req = value
-
-    def get_taskid(self):
-        return self.tid
-
-    def get_taskpriority(self):
-        return self.pr
-
-    def get_arrivaltime(self):
-        return self.at
-
-    def get_et(self):
-        return self.et
-    
-    def get_req(self):
-        return self.req
 
 t = Task(1,0,9,3,2)
 print (t.get_et())
